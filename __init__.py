bfe7605fe4dc2d1d8e855413d8aba95cf1cce08d
Hgg <2130374@qq.com>
2023-02-12 19:15:14 +0800

    初始化

---
+++ __init__.py
@@ -0,0 +1,65 @@
+import httpx
+from nonebot import logger, on_command
+from nonebot.adapters import Message
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg
+tiangou_matcher = on_command("一句")
+meiyan_matcher = on_command("一言")
+dujitang_matcher = on_command("毒鸡汤")
+caihongpi_matcher = on_command("彩虹屁")
+
+
+@tiangou_matcher.handle()
+async def hitokoto(matcher: Matcher, args: Message = CommandArg()):
+    if args:
+        return
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://cloud.qqshabi.cn/api/tiangou/api.php")
+    if response.is_error:
+        logger.error("获取舔狗日记失败")
+        return
+    msg = response.text
+    await matcher.finish(msg)
+
+@meiyan_matcher.handle()
+async def hitokoto(matcher: Matcher, args: Message = CommandArg()):
+    if args:
+        return
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://cloud.qqshabi.cn/api/hitokoto/hitokoto.php")
+    if response.is_error:
+        logger.error("获取美言失败")
+        return
+    msg = response.text
+    await matcher.finish(msg)
+
+@dujitang_matcher.handle()
+async def hitokoto(matcher: Matcher, args: Message = CommandArg()):
+    if args:
+        return
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://cloud.qqshabi.cn/api/poison/api.php")
+    if response.is_error:
+        logger.error("获取毒鸡汤失败")
+        return
+    data2 = response.json()
+    add = ""
+    if msg := data2["data"]:
+         add += f"{msg}"
+    await matcher.finish(msg)
+
+@caihongpi_matcher.handle()
+async def hitokoto(matcher: Matcher, args: Message = CommandArg()):
+    if args:
+        return
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://cloud.qqshabi.cn/api/rainbow/api.php")
+    if response.is_error:
+        logger.error("获取彩虹屁失败")
+        return
+    data1 = response.json()
+    add = ""
+    if msg := data1["data"]:
+         add += f"{msg}"
+    await matcher.finish(add)
+
